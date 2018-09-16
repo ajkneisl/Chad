e6032417387668a6ee78acc7493019cf616797ab
Jake <j4kemc@gmail.com>
2018-09-15 20:44:52 -0600

    I MADE NEW SHIT

--- java/src/com/jhobot/commands/fun/TTT.java
+++ java/src/com/jhobot/commands/fun/TTT.java
@@ -4,8 +4,19 @@ import java.util.Random
 public class TTT {
     public static void faggot(){
         Random rand = new Random();
-        int n = rand.nextInt(30) + 1;
-        System.out.println(n);
+        int n = rand.nextInt(10) + 1;
+        if (n <= 3) {
+            System.out.println("You caught a Jake");
+            else if (n <= 6) {
+                System.out.println("You caught a Sho");
+            }
+            else if (n <= 9){
+                System.out.println("You caught a Diggity");
+            }
+            else if (n == 10){
+                System.out.println("You caught a super rare James!");
+            }
+        }
     }
 
 }
