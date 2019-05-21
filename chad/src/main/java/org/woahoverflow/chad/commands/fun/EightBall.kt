72c4d9202953aa02e162bf496b06e848f46aba50
sho <blaziyt@gmail.com>
2019-05-21 17:17:33 -0500

    Updated eightball

--- chad/src/main/java/org/woahoverflow/chad/commands/fun/EightBall.kt
+++ chad/src/main/java/org/woahoverflow/chad/commands/fun/EightBall.kt
@@ -1,15 +1,10 @@
 package org.woahoverflow.chad.commands.`fun`
 
-import org.json.JSONArray
-import org.woahoverflow.chad.framework.handle.JsonHandler
+import org.woahoverflow.chad.core.ChadVar
 import org.woahoverflow.chad.framework.handle.MessageHandler
 import org.woahoverflow.chad.framework.obj.Command
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageEvent
-import sx.blah.discord.util.EmbedBuilder
-
-import java.security.SecureRandom
-import java.util.HashMap
-import java.util.Objects
+import java.util.*
 
 /**
  * Shake the 8ball
@@ -18,23 +13,12 @@ import java.util.Objects
  */
 class EightBall : Command.Class {
     override suspend fun run(e: MessageEvent, args: MutableList<String>) {
-        val messageHandler = MessageHandler(e.channel, e.author)
-
-        // Makes sure they asked a question
-        if (args.isEmpty()) {
-            messageHandler.sendError("You didn't ask a question!")
-            return
-        }
-
-        // Gets the answers from the cdn
-        val answers = Objects.requireNonNull<JSONArray>(JsonHandler.readArray("https://cdn.woahoverflow.org/data/chad/8ball.json"))
-
-        // Sends the answer
-        messageHandler.sendEmbed(
-                EmbedBuilder().withDesc(
-                        answers.getString(SecureRandom().nextInt(Objects.requireNonNull(answers).length()))
-                )
-        )
+        MessageHandler(e.channel, e.author).also {
+            if (args.isEmpty()) {
+                it.sendError("You didn't ask a question!")
+                return
+            }
+        }.sendEmbed { withDesc(ChadVar.eightBallResults.random()) }
     }
 
     override suspend fun help(e: MessageEvent) {
