9d9947cdf38074bc485233ed1802e753e57122b1
sho <blaziyt@gmail.com>
2019-05-22 20:10:34 -0500

    Updated Chad command

--- chad/src/main/java/org/woahoverflow/chad/commands/info/Chad.kt
+++ chad/src/main/java/org/woahoverflow/chad/commands/info/Chad.kt
@@ -16,19 +16,14 @@ import java.util.*
  */
 class Chad : Command.Class {
     override suspend fun run(e: MessageEvent, args: MutableList<String>) {
-        // Creates an embed builder, and adds links etc to it.
-        val embedBuilder = EmbedBuilder()
-
-        val desc = "**Chad** by woahoverflow\n\n" +
-                "**Uptime** ${Util.fancyDate(ManagementFactory.getRuntimeMXBean().uptime)}\n" +
-                "**Ping** `${e.client.shards[0].responseTime}` ms\n" +
-                "**GitHub** https://woahoverflow.org/github\n\nIf there's an issue, or something needs to be fixed, message shozer#0001 on Discord."
-
-        embedBuilder.withDesc(desc)
-        embedBuilder.withUrl("https://woahoverflow.org/chad")
-
-        // Sends
-        MessageHandler(e.channel, e.author).credit(ChadVar.VERSION).sendEmbed(embedBuilder)
+        MessageHandler(e.channel, e.author).credit(ChadVar.VERSION).sendEmbed {
+            withDesc("**Chad** by woahoverflow\n\n" +
+                    "**Uptime** ${Util.fancyDate(ManagementFactory.getRuntimeMXBean().uptime)}\n" +
+                    "**Ping** `${e.client.shards[0].responseTime}` ms\n" +
+                    "**GitHub** https://woahoverflow.org/github" +
+                    "\n\nIf there's an issue, please join our Discord at https://woahoverflow.org/discord")
+            withUrl("https://woahoverflow.org/chad")
+        }
     }
 
     override suspend fun help(e: MessageEvent) {
